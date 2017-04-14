427e4c6ba79ed8c4e334ce176a0f45c879fc5d4d
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2017-04-13 21:09:05 -0300

    - GitHub Atom

--- br/js/viglet.js
+++ br/js/viglet.js
@@ -1,20 +1,22 @@
-	$.get("https://bitbucket.org/sumware/viglet-turing/rss?token=0cce26757ed49fa69e894668377cffc6", function (data) {
+	$.get("https://viglet.ai/github/atom/turing", function (data) {
 	    var $XML = $(data);
-	    $XML.find("item").each(function () {
+	    $XML.find("entry").each(function () {
 	        var $this = $(this)
 	            , item = {
 	                title: $this.find("title").text()
-	                , link: $this.find("link").text()
+	                , link: $this.find("link").attr("href")
 	                , description: $this.find("description").text()
-	                , pubDate: $this.find("pubDate").text()
-	                , author: $this.find("author").text()
+	                , pubDate: $this.find("updated").text()
+	                , authorName: $this.find("author").find("name").text()
+                    , authorUri: $this.find("author").find("uri").text()
+                
+                    
 	            };
-	        var formats = [
-														'ddd, DD MMM YYYY HH:mm:ss ZZ'
-														, 'ddd, DD MMM YY HH:mm:ss ZZ'];
+	        var formats = ['YYYY-MM-DDTHH:mm:ssZ', 'YYYY-MM-DDTHH:mm:ssZ'];
 	        var date = new Date(item.pubDate);
 	        var strDate = date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear();
 	        var momentDate = moment(item.pubDate, formats).locale('en').fromNow();
-	        $('#turingfeed').append($('<div style="margin-top:20px">').html(item.title.replace(/\n/g,"<br>"))).append($('<small>').html(momentDate));
+	        $('#turingfeed').append($('<div style="margin-top:10px">').html('<a style="color:black" href="' + item.link + '">' + item.title.replace(/\n/g, "<br>") + '</a>')).append($('<small>').html('<a href="' + item.authorUri + '" target="_blank" >' + item.authorName + '</a>' +
+	            " - " + momentDate ));
 	    });
 	});
